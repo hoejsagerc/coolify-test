ffda2cdeeb7765a69ae4d25463b1e77d0339ea3b
hoejsagerc <christian@hoejsager.com>
2024-05-16 21:37:28 +0200

    added new endpoint

--- src/Test.Api/Program.cs
+++ src/Test.Api/Program.cs
@@ -15,6 +15,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () =>
+{
+    return Results.Ok("Healty");
+});
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
